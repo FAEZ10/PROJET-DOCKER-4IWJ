1aa1a84372498ce1a1da0e595fabadaf76320a94
faez <benfaez07@gmail.com>
2024-05-15 16:33:17 +0200

    Insert more todo

--- Db/postgres/initdb/init-db.sql
+++ Db/postgres/initdb/init-db.sql
@@ -4,4 +4,4 @@ CREATE TABLE IF NOT EXISTS todo (
   done BOOLEAN
 );
 
-INSERT INTO todo (title, done) VALUES ('Tâche 1', false), ('Tâche 2', true), ('Tâche 3', false), ('Tâche 4', true), ('Tâche 5', false), ('Tâche 6', true);
+INSERT INTO todo (title, done) VALUES ('Tâche 1', false), ('Tâche 2', true), ('Tâche 3', false), ('Tâche 4', true), ('Tâche 5', false), ('Tâche 6', true), ('Tâche 7', false), ('Tâche 8', true), ('Tâche 9', false), ('Tâche 10', true);
